eff4f3f55c5e707352903a0e5fe143d9ca8bda56
hundred0312 <fangchamps0312@yahoo.com>
2016-10-26 14:59:15 +0800

    Initial Commit

---
+++ Ruler/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ruler
+//
+//  Created by Com on 26/10/2016.
+//  Copyright © 2016 Com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ruler/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ruler
+//
+//  Created by Com on 26/10/2016.
+//  Copyright © 2016 Com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
