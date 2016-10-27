033fca2c92c3d280df4dba600deedcd258c84f4a
hundred0312 <fangchamps0312@yahoo.com>
2016-10-27 17:19:31 +0800

    - created project

--- Ruler/RulerView.h
+++ Ruler/RulerView.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  RulerView.h
 //  Ruler
 //
 //  Created by Com on 26/10/2016.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface RulerView : UIView
 
 @end
-
---
+++ Ruler/RulerView.m
@@ -0,0 +1,21 @@
+//
+//  RulerView.m
+//  Ruler
+//
+//  Created by Com on 26/10/2016.
+//  Copyright © 2016 Com. All rights reserved.
+//
+
+#import "RulerView.h"
+
+@implementation RulerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
